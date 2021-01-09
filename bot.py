1eac2d102db58bd2e4d2efb2fe755164132ef4f3
Nitesh Prajapat <nitesh.19jdml019@jietjodhpur.ac.in>
2021-01-09 12:20:46 +0530

    new feature added

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from typing import Text
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,6 +9,7 @@ def top_trending_news():
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     data = soup.find_all(class_ = "main-title")
+    link_text = ""
     for i in data:        
         result = i.get_text()
         print(result)
